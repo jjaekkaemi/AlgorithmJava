891c246f52f745cb1c4e2204bc959c528d8476ba
jjaekkaemi <35017054+jjaekkaemi@users.noreply.github.com>
2024-09-05 15:36:49 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 196 ms, Memory: 17292 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -4,7 +4,8 @@ import java.io.InputStreamReader;
 import java.util.*;
 public class Main {
     static int n;
-
+    static int answer ;
+    static int[][] arr ;
     static class Pipe {
         int x, y ;
         int direcion ; // 0 : 가로, 1: 대각선, 2: 세로
@@ -20,7 +21,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[n+1][n+1];
+        arr = new int[n+1][n+1];
         for(int i = 1 ; i<n+1 ;i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 1 ; j<n+1 ; j++){
@@ -29,30 +30,27 @@ public class Main {
             }
         }
         Queue<Pipe> queue = new LinkedList<>();
-        int answer = 0 ;
-        queue.offer(new Pipe(1,2, 0));
-        while(!queue.isEmpty()){
-            Pipe pipe = queue.poll();
-            int x = pipe.x;
-            int y = pipe.y ;
-            if(x==n && y==n){
-                answer++;
-                continue;
-            }
-            if(pipe.direcion==0 || pipe.direcion==1){
-                if(y+1<=n && arr[x][y+1]==0) {
-                    queue.offer(new Pipe(x, y+1, 0));
-                }
-            }
-            if(pipe.direcion==1 || pipe.direcion==2){
-                if(x+1<=n && arr[x+1][y]==0){
-                    queue.offer(new Pipe(x+1, y, 2));
-                }
-            }
-            if(x+1<=n && y+1<=n && arr[x+1][y+1]==0 && arr[x+1][y]==0 && arr[x][y+1]==0){
-                queue.offer(new Pipe(x+1, y+1, 1));
+        answer = 0 ;
+        dfs(1,2,0);
+        System.out.println(answer);
+    }
+    static void dfs(int x, int y, int direction){
+        if(x==n && y==n){
+            answer++;
+            return;
+        }
+        if(direction==0 || direction==1){
+            if(y+1<=n && arr[x][y+1]==0) {
+                dfs(x, y+1, 0);
             }
         }
-        System.out.println(answer);
+        if(direction==1 || direction==2){
+            if(x+1<=n && arr[x+1][y]==0){
+                dfs(x+1, y, 2);
+            }
+        }
+        if(x+1<=n && y+1<=n && arr[x+1][y+1]==0 && arr[x+1][y]==0 && arr[x][y+1]==0){
+            dfs(x+1, y+1, 1);
+        }
     }
 }
