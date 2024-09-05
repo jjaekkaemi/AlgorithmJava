490abb8ad2d6b08908ac349464c513ec4297a1fd
jjaekkaemi <35017054+jjaekkaemi@users.noreply.github.com>
2024-09-05 15:33:54 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 928 ms, Memory: 497892 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -2,67 +2,57 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-
 public class Main {
     static int n;
-    
-    static class Pipe {
-        int x, y, direction; // 머리 좌표 (x, y) 및 방향 (0: 가로, 1: 대각선, 2: 세로)
 
-        public Pipe(int x, int y, int direction) {
+    static class Pipe {
+        int x, y ;
+        int direcion ; // 0 : 가로, 1: 대각선, 2: 세로
+
+        public Pipe(int x, int y, int direcion) { // 시작 좌표, 뒤 좌표, 방향
             this.x = x;
             this.y = y;
-            this.direction = direction;
+
+            this.direcion = direcion;
         }
     }
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
         int[][] arr = new int[n+1][n+1];
-        
-        for (int i = 1; i <= n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= n; j++) {
+        for(int i = 1 ; i<n+1 ;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1 ; j<n+1 ; j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
+
             }
         }
-
         Queue<Pipe> queue = new LinkedList<>();
-        queue.offer(new Pipe(1, 2, 0)); // 파이프는 처음에 (1,1)에서 (1,2)까지 가로 방향으로 놓여있음
-        int answer = 0;
-
-        while (!queue.isEmpty()) {
+        int answer = 0 ;
+        queue.offer(new Pipe(1,2, 0));
+        while(!queue.isEmpty()){
             Pipe pipe = queue.poll();
             int x = pipe.x;
-            int y = pipe.y;
-            int direction = pipe.direction;
-
-            if (x == n && y == n) { // 도착지점에 도달
+            int y = pipe.y ;
+            if(x==n && y==n){
                 answer++;
                 continue;
             }
-
-            // 가로 방향 (0)
-            if (direction == 0 || direction == 1) {
-                if (y + 1 <= n && arr[x][y + 1] == 0) { // 가로로 이동
-                    queue.offer(new Pipe(x, y + 1, 0));
+            if(pipe.direcion==0 || pipe.direcion==1){
+                if(y+1<=n && arr[x][y+1]==0) {
+                    queue.offer(new Pipe(x, y+1, 0));
                 }
             }
-
-            // 세로 방향 (2)
-            if (direction == 2 || direction == 1) {
-                if (x + 1 <= n && arr[x + 1][y] == 0) { // 세로로 이동
-                    queue.offer(new Pipe(x + 1, y, 2));
+            if(pipe.direcion==1 || pipe.direcion==2){
+                if(x+1<=n && arr[x+1][y]==0){
+                    queue.offer(new Pipe(x+1, y, 2));
                 }
             }
-
-            // 대각선 방향 (1)
-            if (x + 1 <= n && y + 1 <= n && arr[x + 1][y] == 0 && arr[x][y + 1] == 0 && arr[x + 1][y + 1] == 0) {
-                queue.offer(new Pipe(x + 1, y + 1, 1)); // 대각선으로 이동
+            if(x+1<=n && y+1<=n && arr[x+1][y+1]==0 && arr[x+1][y]==0 && arr[x][y+1]==0){
+                queue.offer(new Pipe(x+1, y+1, 1));
             }
         }
-
         System.out.println(answer);
     }
 }
